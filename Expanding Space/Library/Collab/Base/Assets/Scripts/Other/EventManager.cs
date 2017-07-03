bd74dd4b86f787860e279acd894b882ecae8f8f5
projojoboy <joeykossen@hotmail.com>
2017-07-03 13:40:38 +0200

    Upload Game

---
+++ Expanding Space/Library/Collab/Base/Assets/Scripts/Other/EventManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public delegate void StartGameDelegate();
+    public static StartGameDelegate onStartGame;
+    public static StartGameDelegate onInstructionsGame;
+    public static StartGameDelegate onBackGame;
+    public static StartGameDelegate onNextGame;
+    public static StartGameDelegate onPreviousGame;
+    public static StartGameDelegate onPlayerDeath;
+    public static StartGameDelegate onRespawnPickup;
+
+    public delegate void TakeDamageDelegate(float amt);
+    public static TakeDamageDelegate onTakeDamage;
+
+    public delegate void ScorePointsDelegate(int amt);
+    public static ScorePointsDelegate onScorePoints;
+
+    public static void StartGame()
+    {
+       // Debug.Log("Start Game");
+        if(onStartGame != null)
+        {
+            onStartGame();
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    public static void InstructionsGame()
+    {
+         //Debug.Log("Instructions from Game");
+        if (onInstructionsGame != null)
+        {
+            onInstructionsGame();
+        }
+    }
+
+
+    public static void BackGame()
+    {
+        //Debug.Log("Back to menu");
+        if (onBackGame != null)
+        {
+            onBackGame();
+        }
+    }
+
+    public static void NextGame()
+    {
+        //Debug.Log("Back to menu");
+        if (onNextGame != null)
+        {
+            onNextGame();
+        }
+    }
+
+    public static void PreviousGame()
+    {
+        //Debug.Log("Back to menu");
+        if (onPreviousGame != null)
+        {
+            onPreviousGame();
+        }
+    }
+
+
+    public static void QuitGame()
+    {
+         //Debug.Log("Quit Game");
+         Application.Quit();
+    }
+
+
+    public static void ReSpawnPickup()
+    {
+        // Debug.Log("Start Game");
+        if (onRespawnPickup != null)
+        {
+            onRespawnPickup();
+        }
+    }
+
+
+    public static void TakeDamage(float percent)
+    {
+      //  Debug.Log("Take Damage: " + percent);
+        if (onTakeDamage != null)
+        {
+            onTakeDamage(percent);
+        }
+    }
+
+
+    public static void PlayerDeath()
+    {
+        //Debug.Log("Player Died");
+        if (onPlayerDeath != null)
+        {
+            onPlayerDeath();
+
+
+        }
+
+    }
+
+    public static void ScorePoints(int score)
+    {
+        //  Debug.Log("Take Damage: " + percent);
+        if (onScorePoints != null)
+        {
+            onScorePoints(score);
+        }
+    }
+
+
+}
---
+++ Expanding Space/Library/Collab/Download/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    Laser[] laser;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKey("joystick button 0"))
+        {
+            foreach (Laser l in laser)
+            {
+                //  Vector3 pos = transform.position + (transform.forward * l.Distance);
+                l.FireLaser();
+            }
+        }
+    }
+}
